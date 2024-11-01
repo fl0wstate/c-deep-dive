6bf27d379c4a23c6ae9d45774181699880bd9591
gh057 <victormurithi2020@gmail.com>
2024-11-01 18:04:21 +0300

    fix: Applied proper linting

--- macros.c
+++ macros.c
@@ -6,7 +6,10 @@
 /* Check for the system bit field */
 #define BITARRY_LEN ((sizeof(unsigned)) * (CHAR_BIT))
 /* Set a bit at a particular index in the bit array */
-#define SET_BIT(K, BITARRY) ( BITARRY[(K) / (BITARRY_LEN)] |= (1 << ((K) % (BITARRY_LEN))) )
-#define CLEAR_BIT(K, BITARRY) ( BITARRY[(K) / (BITARRY_LEN)] &= ~(1 << ((K) % (BITARRY_LEN))) )
-#define CHECK_BIT(K, BITARRY) ( BITARRY[(K) / (BITARRY_LEN)] & (1 << ((K) % (BITARRY_LEN))) )
+#define SET_BIT(K, BITARRY)                                                    \
+  (BITARRY[(K) / (BITARRY_LEN)] |= (1 << ((K) % (BITARRY_LEN))))
+#define CLEAR_BIT(K, BITARRY)                                                  \
+  (BITARRY[(K) / (BITARRY_LEN)] &= ~(1 << ((K) % (BITARRY_LEN))))
+#define CHECK_BIT(K, BITARRY)                                                  \
+  (BITARRY[(K) / (BITARRY_LEN)] & (1 << ((K) % (BITARRY_LEN))))
 #endif
