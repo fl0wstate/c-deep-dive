228bc7553a9b37b31821f3b91c5dece24d39121c
gh057 <victormurithi2020@gmail.com>
2025-03-08 09:32:34 +0000

    fix: PROMPT is not working correctly

--- ftp-server/log.c
+++ ftp-server/log.c
@@ -12,33 +12,39 @@ void LOG(logll level, const char *format, ...)
   strftime(timestamp, 20, "%Y-%m-%d %H:%M:%S", timeinfo);
 
   switch (level)
-    {
-    case INFO:
-      fprintf(stdout, ANSI_COLOR_GREEN "%s:[INFO]", timestamp);
-      break;
-    case DEBUG:
-      fprintf(stdout, ANSI_COLOR_CYAN "%s:[DEBUG]", timestamp);
-      break;
-    case ERROR:
-      fprintf(stderr, ANSI_COLOR_RED "%s:[ERROR]", timestamp);
-      break;
-    }
+  {
+  case INFO:
+    fprintf(stdout, ANSI_COLOR_GREEN "%s:[INFO]", timestamp);
+    break;
+  case PROMPT:
+    fprintf(stdout, ANSI_COLOR_YELLOW "%s:[SHELL]", timestamp);
+    break;
+  case DEBUG:
+    fprintf(stdout, ANSI_COLOR_CYAN "%s:[DEBUG]", timestamp);
+    break;
+  case ERROR:
+    fprintf(stderr, ANSI_COLOR_RED "%s:[ERROR]", timestamp);
+    break;
+  }
 
   va_start(args, format);
   switch (level)
-    {
-    case INFO:
-      vfprintf(stdout, format, args);
-      fprintf(stdout, "\n");
-      break;
-    case DEBUG:
-      vfprintf(stdout, format, args);
-      fprintf(stdout, "\n");
-      break;
-    case ERROR:
-      vfprintf(stderr, format, args);
-      fprintf(stderr, "\n");
-      break;
-    }
+  {
+  case INFO:
+    vfprintf(stdout, format, args);
+    fprintf(stdout, "\n");
+    break;
+  case DEBUG:
+    vfprintf(stdout, format, args);
+    fprintf(stdout, "\n");
+    break;
+  case PROMPT:
+    vfprintf(stdout, format, args);
+    break;
+  case ERROR:
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+    break;
+  }
   va_end(args);
 }
