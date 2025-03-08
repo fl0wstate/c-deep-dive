0f026d3ab2164c4aa0d89b8860d18916d332e843
gh057 <victormurithi2020@gmail.com>
2025-03-08 09:31:51 +0000

    feat: got a simple working shell demo

--- ftp-server/cli_ftp_client.c
+++ ftp-server/cli_ftp_client.c
@@ -1,12 +1,77 @@
 #include "ftp.h"
 
+char *_getline(int len, FILE *stream)
+{
+  size_t inital_size = (!len) ? SMALL_BUFF : len, i = 0;
+  int count = 0;
+  ssize_t bytes_read = 0;
+
+  char *command_line_buffer;
+  // allocate memory to the local buffer
+  command_line_buffer = malloc(inital_size);
+  if (!command_line_buffer)
+  {
+    LOG(ERROR,
+        "<FTP_LOG> Failed to allocate memory to the command line buffer @line "
+        "[%d]",
+        __LINE__);
+    return NULL;
+  }
+  while ((bytes_read = read(stream->_fileno, &(command_line_buffer)[i++], 1)) >
+         REOF)
+  {
+    // if the storage is not enough realocate more storage
+    if (i >= inital_size)
+    {
+      count++;
+      inital_size *= 2;
+      command_line_buffer = realloc(command_line_buffer, inital_size);
+      if (!command_line_buffer)
+      {
+        LOG(ERROR,
+            "<FTP_LOG> Failed to allocate memory to the command line buffer "
+            "@line "
+            "[%d]",
+            __LINE__);
+        return NULL;
+      }
+    }
+    if (command_line_buffer[i - 1] == '\n' || command_line_buffer[i] == EOF)
+      break;
+  }
+
+  command_line_buffer[i] = '\0';
+
+  if (count)
+    LOG(DEBUG, "This is the number of reallocation [%d]", count);
+
+  return command_line_buffer;
+}
+
+void ftp_cli_parser()
+{
+  while (1)
+  {
+    char *command_line;
+    // char **command_line_args;
+
+    fprintf(stdout, "ftp>> ");
+    fflush(stdout);
+    command_line = _getline(SMALL_BUFF, stdin);
+    if (!command_line)
+    {
+      LOG(ERROR, "Parser Failed closing this interactive ftp shell");
+      exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, ANSI_COLOR_YELLOW "[OUT] %s" ANSI_RESET_ALL, command_line);
+
+    free(command_line);
+    // free(command_line_args);
+  }
+}
 int main(int argc, char *argv[])
 {
 
-  while (1)
-  {
-    LOG(INFO, "<ftp>: ");
-  }
-
+  ftp_cli_parser();
   return EXIT_SUCCESS;
 }
